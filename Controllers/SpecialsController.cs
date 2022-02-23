35ff4fb3335b59572e83a957dcb3bc0ffe1f4219
FJRuiz <fjruizperez@marbella.es>
2022-02-23 20:44:00 +0100

    Avance sustancial

---
+++ Controllers/SpecialsController.cs
@@ -0,0 +1,26 @@
+﻿using BlazingPizza.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlazingPizza.Controllers
+{
+    [Route("specials")]
+    [ApiController]
+    public class SpecialsController : ControllerBase
+    {
+        private readonly PizzaStoreContext _context;
+        public SpecialsController(PizzaStoreContext context)
+        {
+            _context = context; 
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+        {
+            var response = new List<PizzaSpecial>();
+            response = (await _context.Specials.ToListAsync()).OrderBy(s => s.BasePrice).ToList();
+            return response;
+        }
+    }
+}
